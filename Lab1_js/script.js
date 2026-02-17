330047bcf1c9c8b0be06f5d6efd477587ba5caf2
Arina Petrovscaia <ckalovaarina@gmail.com>
2026-02-17 23:32:44 +0200

    Initial commit

---
+++ Lab1_js/script.js
@@ -0,0 +1,23 @@
+// alert("Этот код выполнен из внешнего файла!");
+// console.log("Hello!");
+
+let name = 'Arina';
+let birthYear = 2004;
+let isStudent = true;
+
+console.log(name);
+console.log(birthYear);
+console.log(isStudent);
+
+let score = prompt("Введите ваш балл:");
+if (score >= 90) {
+    console.log("Отлично!");
+} else if (score >= 70) {
+    console.log("Хорошо");
+} else {
+    console.log("Можно лучше!");
+}
+
+for (let i = 1; i <= 5; i++) {
+    console.log(`Итерация: ${i}`);
+}
